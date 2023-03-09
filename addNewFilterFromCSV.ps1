0b46a377f87e6d78421cc426412eba5909808cef
Bartosz Sporek <bartoszspor@gmail.com>
2023-03-09 14:40:22 +0100

    Initial build

---
+++ addNewFilterFromCSV.ps1
@@ -0,0 +1,11 @@
+﻿Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Bypass
+
+$employee_list = Import-Csv C:\Path\To\list.csv
+
+foreach ($employee in $employee_list) {
+    gam user $employee.Email add forwardingaddress example@gmail.com 
+    
+    gam user $employee.Email add filter from "-(@dropbox.com OR @mailing.archibald.pl OR @ifeelonline.com OR @automation.atlassian.com OR @shl.com OR @elucidat.com OR @demodesk.com OR @docs.google.com OR @pekao.com.pl OR @gowork.pl OR @saplingapp.io OR @em-s.dropbox.com OR @makeup.pl OR @pointnine.com)" forward example@gmail.com 
+    
+}
+Read-Host -Prompt "Press any key to continue"
---
+++ filtersMatch.ps1
@@ -0,0 +1,15 @@
+﻿Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Bypass
+
+# Imports .csv list
+$employee_list = Import-Csv C:\Users\Bartek\Desktop\list.csv
+
+# Iterates over employees list, assigns Gmail filters to variable and checks if any of those contains given email 
+foreach ($employee in $employee_list) {
+    $emailFilters = gam user $employee.Email print filters 2> $null 
+    
+    if ($emailFilters -match "example@gmail.com"){
+	  
+        Write-Host $employee.Email `n $emailFilters `n`n 
+    }
+}
+Read-Host -Prompt "Press any key to continue"
